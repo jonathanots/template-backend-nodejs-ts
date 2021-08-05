14396713bd35227954b0bdc68587eed727d7fe82
jsouzayoobot <jsouza@yoobot.com.br>
2021-08-05 09:58:31 -0300

    [FIX] Fixed date field in Order class, then refactory variables nullable

--- src/entities/order/index.ts
+++ src/entities/order/index.ts
@@ -1,11 +1,11 @@
 export class OrderResponse {
-    public id!: string;
-    public company_id!: string;
-    public customer_id!: string | null;
-    public status_id!: string;
-    public method_order_id!: string;
-    public payment_id!: string;
-    public description!: string;
+    public id?: string;
+    public company_id?: string;
+    public customer_id?: string | null;
+    public status_id?: string;
+    public method_order_id?: string;
+    public payment_id?: string;
+    public description?: string;
 
     constructor(props: OrderResponse) {
         Object.assign(this, props);
@@ -13,23 +13,27 @@ export class OrderResponse {
 }
 
 export class Order {
-    public order_id!: string;
-    public company_id!: string;
-    public customer_id!: string | null;
-    public status_id!: string;
-    public method_order_id!: string;
-    public payment_id!: string;
-    public description!: string;
-    public products!: OrderProduct[];
+    public order_id?: string;
+    public company_id?: string;
+    public customer_id?: string | null;
+    public status_id?: string;
+    public method_order_id?: string;
+    public payment_id?: string;
+    public description?: string;
+    public products?: OrderProduct[];
+    public date? : Date;
 
-    constructor(props: Order) {
+    constructor(props: Order, date? :Date) {
         Object.assign(this, props);
+
+        if(!this.date)
+            this.date = new Date();
     }
 }
 
 export class OrderProduct {
-    public order_id!: string;
-    public catalog_id!: string;
+    public order_id?: string;
+    public catalog_id?: string;
 
     constructor(props: OrderProduct) {
         Object.assign(this, props);
