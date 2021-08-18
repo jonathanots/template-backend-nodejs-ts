8a687266cf7bf83016db792a36d864bff6305dca
jsouzayoobot <jsouza@yoobot.com.br>
2021-08-18 20:59:54 -0300

    #2 [FIX] Logs directory changed to root at project and fixed path to add logs files

--- src/app/logger/index.ts
+++ src/app/logger/index.ts
@@ -2,8 +2,8 @@ import morgan from "morgan";
 import fs from "fs";
 import path from "path";
 
-const folder = '/logs';
+const folder = 'logs/';
 
-const accessLogStream = fs.createWriteStream(path.join(__dirname, folder, 'access.log'), { flags: 'a' });
+const accessLogStream = fs.createWriteStream(path.join(folder, 'access.log'), { flags: 'a' });
 
 export const logger = morgan('common', {stream: accessLogStream});
